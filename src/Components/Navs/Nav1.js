f728a4e5771d572b8f090a47eeb8e26840dcd367
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-06 09:57:47 +0530

    uploding all the necessary files plus started doing Nav

---
+++ src/Components/Navs/Nav1.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Nav1() {
+  return (
+    <div>
+      <div class="nav">
+        <div class="nav_left">
+          <div class="nav_logo">
+            <img
+              src="https://static.cure.fit/assets/images/curefit-v-man.svg"
+              alt=""
+            />
+          </div>
+          <div class="nav_name">
+            <h2>Gym .Fit</h2>
+          </div>
+        </div>
+        <div class="nav_center">
+          <ul>
+            <li>Gym Centers</li>
+            <li>Gyms</li>
+            <li>Online PT</li>
+            <li>Gym Sport</li>
+            <li>Premium Gym</li>
+          </ul>
+        </div>
+        <div class="nav_right">
+          <div class="nav_login">
+            <i class="fas fa-user"></i>
+            <span>Login</span>
+          </div>
+          <div class="nav_cart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav1;
