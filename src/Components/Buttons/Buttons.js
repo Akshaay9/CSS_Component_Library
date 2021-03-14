3735383298e0e3cdf9ca2c4702108b660a02b4f7
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-14 11:34:52 +0530

    added different CSS hover Effect

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -3,14 +3,14 @@ import React from 'react'
 function Buttons() {
     return (
         <div style={{display:"grid",gridTemplateColumns:"repeat(3,1fr)",gridColumnGap:"15px",gridRowGap:"15px"}}>
-        <button className="btn btn-primary">Primary</button>
-        <button className="btn btn-secondary">secondary</button>
-        <button className="btn btn-success">success</button>
-        <button className="btn btn-danger">danger</button>
+        <button className="btn btn-primary btn-primary-hr-outline-out">Primary</button>
+        <button className="btn btn-secondary btn-secondary-hr-outline-in">secondary</button>
+        <button className="btn btn-success btn-success-hr-scale-out">success</button>
+        <button className="btn btn-danger btn-danger-hr-outline-out">danger</button>
         <button className="btn btn-info">info</button>
-        <button className="btn btn-light">light</button>
+        <button className="btn btn-light btn-primary btn-light-hr-outline-out">light</button>
         <button className="btn btn-dark">dark</button>
-        <button className="btn btn-link">link</button>       
+        <button className="btn btn-link btn-link-hr-outline-in-thick">link</button>       
         </div>
     )
 }
