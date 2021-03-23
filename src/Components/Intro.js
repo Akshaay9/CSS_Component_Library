5251bcf9475f1fea83fff2d3559c69bcc46ba685
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-23 20:55:14 +0530

    made changes to the intro page

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -5,11 +5,11 @@ function Intro() {
         <>
         <div style={{ marginTop: "3rem", marginLeft: "20px" }}>
             <h2  >Welcome to UI world.</h2>
-<p style={{width:"70%",fontSize:"17px",lineHeight:"24px",marginTop:"12px"}}>Start Building web applications without worrying about style,desgn and color, let me take of that, Introducing you, UI world. A CSS component library with more 10+ popular Style components specially designed by keeping in mind all the necessary design principles.</p>
+<p style={{width:"70%",fontSize:"17px",lineHeight:"24px",marginTop:"12px"}}>Start Building web applications like its 2025, without worrying about style,design and color, let me take care of that, Introducing to you, UI world. A CSS component library with more than 10+ popular Styled components specially designed by keeping in mind all the necessary design principles.</p>
             </div>
         <div style={{ marginTop: "1rem", marginLeft: "20px" }}>
             <h2  >How to Integrate UI World in your next Project ?</h2>
-<p style={{width:"70%",fontSize:"17px",lineHeight:"24px",marginTop:"12px"}}> Well It is pretty simple, just copy the above Css line and paste it in your Head of your HTML file.</p>
+<p style={{width:"70%",fontSize:"17px",lineHeight:"24px",marginTop:"12px"}}> Well It is pretty simple, just copy the below CSS line and paste it in your Head Component of your HTML file.</p>
             </div>
             <iframe
   src="https://carbon.now.sh/embed?bg=rgba%28216%2C217%2C218%2C1%29&t=blackboard&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=34px&ph=23px&ln=true&fl=1&fm=Fira+Code&fs=13px&lh=181%25&si=false&es=2x&wm=false&code=%253Clink%2520rel%253D%2522stylesheet%2522%2520href%253D%2522https%253A%252F%252Fakshaay9.github.io%252FcssCompLibrary%252Fsrc%252FApp.css%2522%253E"
@@ -18,7 +18,7 @@ function Intro() {
             </iframe>
             <div style={{ marginTop: "1rem", marginLeft: "20px" }}>
             <h2  >Is the UI World keep getting any updates ?</h2>
-                <p style={{ width: "70%", fontSize: "17px", lineHeight: "24px", marginTop: "12px" }}> Yes. It will be getting Updates frequently, right now i have only added all the necessary/important components one looks for while building an web app, i keep updating this library as i go on creating my projects or when i come acrosee any interesting Styled component, Till then stay tuned</p>
+                <p style={{ width: "70%", fontSize: "17px", lineHeight: "24px", marginTop: "12px" }}> Yes. It will be getting Updates frequently, right now i have  added all the necessary/important components one looks for while building an web app, i keep updating this library as i go on creating my projects or when i come acrosee any interesting Styled component, Till then stay tuned</p>
                 <div style={{display:"flex",width:"15%",justifyContent:"space-between"}}>
                     <a style={{textDecoration:"none"}} href="https://github.com/Akshaay9/cssCompLibrary">GitHub</a>
                     <a style={{textDecoration:"none"}} href="https://twitter.com/Kshy_nair">Twitter</a>
