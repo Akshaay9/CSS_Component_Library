a97dd53755f01de73dfd55d8f11dd40a219fea48
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-25 13:01:52 +0530

    added design to introduction, made intro, avatar and footer responsive

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,33 +1,121 @@
-import React from 'react'
+import React from "react";
 
 function Intro() {
-    return (
-        <>
-        <div style={{ marginTop: "3rem", marginLeft: "20px" }}>
-            <h2  >Welcome to UI world.</h2>
-<p style={{width:"100%",fontSize:"17px",lineHeight:"24px",marginTop:"12px"}}>Start Building web applications like its 2025, without worrying about style,design and color, let me take care of that, Introducing to you, UI world. A CSS component library with more than 10+ popular Styled components specially designed by keeping in mind all the necessary design principles.</p>
-            </div>
-        <div style={{ marginTop: "1rem", marginLeft: "20px" }}>
-            <h2  >How to Integrate UI World in your next Project ?</h2>
-<p style={{width:"100%",fontSize:"17px",lineHeight:"24px",marginTop:"12px"}}> Well It is pretty simple, just copy the below CSS line and paste it in your Head Component of your HTML file.</p>
-            </div>
-            <iframe
-  src="https://carbon.now.sh/embed?bg=rgba%28216%2C217%2C218%2C1%29&t=blackboard&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=34px&ph=23px&ln=true&fl=1&fm=Fira+Code&fs=13px&lh=181%25&si=false&es=2x&wm=false&code=%253Clink%2520rel%253D%2522stylesheet%2522%2520href%253D%2522https%253A%252F%252Fakshaay9.github.io%252FcssCompLibrary%252Fsrc%252FApp.css%2522%253E"
-  style={{width:"700px",height:"190px",display:"block",margin:"auto",marginTop:"18px"}}
-  sandbox="allow-scripts allow-same-origin">
-            </iframe>
-            <div style={{ marginTop: "1rem", marginLeft: "20px" }}>
-            <h2  >Is the UI World keep getting any updates ?</h2>
-                <p style={{ width: "100%", fontSize: "17px", lineHeight: "24px", marginTop: "12px" }}> Yes. It will be getting Updates frequently, right now i have  added all the necessary/important components one looks for while building an web app, i keep updating this library as i go on creating my projects or when i come acrosee any interesting Styled component, Till then stay tuned</p>
-                <div style={{display:"flex",width:"15%",justifyContent:"space-between"}}>
-                    <a style={{textDecoration:"none"}} href="https://github.com/Akshaay9/cssCompLibrary">GitHub</a>
-                    <a style={{textDecoration:"none"}} href="https://twitter.com/Kshy_nair">Twitter</a>
-                    <a style={{textDecoration:"none"}} href="https://t.co/LG8Ki2gZL2?amp=1">LinkedIn</a>
-                    </div>
-            </div>
-           
-            </>
-    )
+  return (
+    <div className="svgBG">
+      <div
+        style={{
+          textAlign: "center",
+          fontFamily: "sans-serif",
+          padding: "3rem",
+        }}
+      >
+        <h1>Welcome to UI world</h1>
+        <p
+          style={{
+            width: "100%",
+            fontSize: "17px",
+            lineHeight: "26px",
+            marginTop: "12px",
+            fontWeight: "thin",
+          }}
+        >
+          Start Building web applications like its 2025, without worrying about
+          style,design and color, let me take care of that, Introducing to you,
+          UI world. A CSS component library with more than 10+ popular Styled
+          components specially designed by keeping in mind all the necessary
+          design principles.
+        </p>
+      </div>
+      <div
+        style={{
+          marginLeft: "20px",
+          textAlign: "center",
+          fontFamily: "sans-serif",
+        }}
+      >
+        <h2>How to Integrate UI World in your next Project ?</h2>
+        <p
+          style={{
+            width: "100%",
+            fontSize: "17px",
+            lineHeight: "24px",
+            marginTop: "12px",
+          }}
+        >
+          {" "}
+          Well It is pretty simple, just copy the below CSS line and paste it in
+          your Head Component of your HTML file.
+        </p>
+          </div>
+          <iframe src="https://www.thiscodeworks.com/embed/605c32722476f700148cf8a9" style="width: 100%; height: 99px;" frameborder="0"
+              style={{ width: "50%", height: "53px", display: "block", margin: "1rem auto", border: "none", minWidth: "20rem" }}
+          ></iframe>
+         
+          
+
+      <div
+        style={{
+          marginLeft: "20px",
+          textAlign: "center",
+          fontFamily: "sans-serif",
+          padding: "1.5rem",
+        }}
+      >
+        <h2>Is the UI World keep getting any updates ?</h2>
+        <p
+          style={{
+            width: "100%",
+            fontSize: "17px",
+            lineHeight: "24px",
+            marginTop: "12px",
+          }}
+        >
+          {" "}
+          Yes. It will be getting Updates frequently, right now i have added all
+          the necessary/important components one looks for while building an web
+          app, i keep updating this library as i go on creating my projects or
+          when i come acrosee any interesting Styled component, Till then stay
+          tuned
+        </p>
+        <div
+          style={{
+            display: "flex",
+                      width: "30%",
+                      minWidth: "19rem",
+                
+            justifyContent: "space-between",
+            margin: "1rem auto",
+          }}
+        >
+          <a
+            style={{ textDecoration: "none" }}
+            href="https://github.com/Akshaay9/cssCompLibrary"
+          >
+            <button className="btn btn-primary btn-primary-hr-outline-out">
+              Git Hub
+            </button>
+          </a>
+          <a
+            style={{ textDecoration: "none" }}
+            href="https://twitter.com/Kshy_nair"
+          >
+            <button className="btn btn-primary btn-primary-hr-outline-out">
+              Twitter
+            </button>
+          </a>
+          <a
+            style={{ textDecoration: "none" }}
+            href="https://t.co/LG8Ki2gZL2?amp=1"
+          >
+            <button className="btn btn-primary btn-primary-hr-outline-out">
+              Linked-In
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Intro
+export default Intro;
--- src/Components/NavForDoc/NavForDoc.js
+++ src/Components/NavForDoc/NavForDoc.js
@@ -12,21 +12,21 @@ function NavForDoc({ setShowCssComp, showcssComp }) {
                     <h3>Ui World</h3>
                 </div>
                 <ul  className={showNavForDoc ?"showResponsiveNav":null} >
-                   <li  onClick={()=>setShowCssComp("welcome")}>Intro</li>
-                    <li onClick={() => setShowCssComp("avatar")}>Avatar</li>
-                    <li onClick={()=>setShowCssComp("nav")}>Nav</li>
-                    <li onClick={()=>setShowCssComp("footer")}>Footer</li>
-                    <li  onClick={()=>setShowCssComp("alert")}>Alert</li>
-                    <li  onClick={()=>setShowCssComp("badge")}>Badge</li>
-                    <li  onClick={()=>setShowCssComp("buttons")}>Buttons</li>
-                    <li  onClick={()=>setShowCssComp("cards")}>Cards</li>
-                    <li  onClick={()=>setShowCssComp("input")}>Input</li>
-                    <li  onClick={()=>setShowCssComp("list")}>List</li>
-                    <li  onClick={()=>setShowCssComp("modal")}>Modal</li>
-                    <li  onClick={()=>setShowCssComp("textutility")}>TextUtilities</li>
-                    <li  onClick={()=>setShowCssComp("toast")}>Toast</li>
-                    <li  onClick={()=>setShowCssComp("DropDownList")}>Drop Down Links</li>
-                    <li  onClick={()=>setShowCssComp("ResponsiveImage")}>Responsive Image</li>
+                    <li onClick={() => { setShowCssComp("welcome");setShowNavForDoc(false)}}>Intro</li>
+                    <li onClick={() => { setShowCssComp("avatar"); setShowNavForDoc(false) }}>Avatar</li>
+                    <li onClick={()=>{setShowCssComp("nav");setShowNavForDoc(false)}}>Nav</li>
+                    <li onClick={()=>{setShowCssComp("footer");setShowNavForDoc(false)}}>Footer</li>
+                    <li  onClick={()=>{setShowCssComp("alert");setShowNavForDoc(false)}}>Alert</li>
+                    <li  onClick={()=>{setShowCssComp("badge");setShowNavForDoc(false)}}>Badge</li>
+                    <li  onClick={()=>{setShowCssComp("buttons");setShowNavForDoc(false)}}>Buttons</li>
+                    <li  onClick={()=>{setShowCssComp("cards");setShowNavForDoc(false)}}>Cards</li>
+                    <li  onClick={()=>{setShowCssComp("input");setShowNavForDoc(false)}}>Input</li>
+                    <li  onClick={()=>{setShowCssComp("list");setShowNavForDoc(false)}}>List</li>
+                    <li  onClick={()=>{setShowCssComp("modal");setShowNavForDoc(false)}}>Modal</li>
+                    <li  onClick={()=>{setShowCssComp("textutility");setShowNavForDoc(false)}}>TextUtilities</li>
+                    <li onClick={() => { setShowCssComp("toast"); setShowNavForDoc(false) }}>Toast</li>
+                    <li onClick={() => { setShowCssComp("DropDownList"); setShowNavForDoc(false) }}>Drop Down Links</li>
+                    <li  onClick={()=>{setShowCssComp("ResponsiveImage");setShowNavForDoc(false)}}>Responsive Image</li>
                    
                 </ul>
             </div>
