b80dd2d28ea0566798ab7745a245a88383b534ca
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-25 14:17:21 +0530

    added responsiveness to almost all the components

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -46,11 +46,7 @@ function Modal() {
         </div>
       </div>
 
-      <iframe
-  src="https://carbon.now.sh/embed?bg=rgba%28216%2C217%2C218%2C1%29&t=blackboard&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=34px&ph=23px&ln=true&fl=1&fm=Fira+Code&fs=13px&lh=181%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-top%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-top-title-left%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Ch3%253EModal%2520Title%253C%252Fh3%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-top-title-right%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Ch3%253Ex%253C%252Fh3%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-body%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Lorem%2520ipsum%2520dolor%2520sit%2520amet%2520consectetur%2520adipisicing%2520elit.%2520Animi%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520pariatur%2520quibusdam%2520temporibus%252C%2520iste%2520necessitatibus%2520nemo%2520non%2520aliquam%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520quos%2520ducimus%2520officia%21%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fp%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-bottom%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-bottom-left%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cbutton%2520className%253D%2522btn%2520btn-dark%2522%253EClose%253C%252Fbutton%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-bottom-right%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cbutton%2520className%253D%2522btn%2520btn-primary%2522%253ESave%2520Changes%253C%252Fbutton%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E"
-  style={{width:"720px",height:"740px",border:"0",overflow:"hidden",display:"block",margin:"auto"}}
-  sandbox="allow-scripts allow-same-origin">
-</iframe>
+      <iframe src="https://www.thiscodeworks.com/embed/605c462b2476f700148cf8bd" style={{ width: "70%", height: "23rem", display: "block", margin: "1rem auto", border: "none", minWidth: "23rem" }}></iframe>
 
 
       <div className="test" style={{ marginTop: "12px" }}></div>
@@ -66,11 +62,7 @@ function Modal() {
           <button className="btn btn-info">Save</button>
         </div>
       </div>
-      <iframe
-  src="https://carbon.now.sh/embed?bg=rgba%28216%2C217%2C218%2C1%29&t=blackboard&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=34px&ph=23px&ln=true&fl=1&fm=Fira+Code&fs=13px&lh=181%25&si=false&es=2x&wm=false&code=%2520%253Cdiv%2520className%253D%2522modal-basic%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-basic-title%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Ch3%253EModal%2520Title%253C%252Fh3%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-basic-body%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Lorem%2520ipsum%2520dolor%2520sit%252C%2520amet%2520consectetur%2520adipisicing%2520elit.%2520Tenetur%252C%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520repudiandae%21%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520className%253D%2522modal-basic-bottom%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cbutton%2520className%253D%2522btn%2520btn-info%2522%253ESave%253C%252Fbutton%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E"
-  style={{width:"720px",height:"440px",border:"0",overflow:"hidden",display:"block",margin:"auto"}}
-  sandbox="allow-scripts allow-same-origin">
-</iframe>
+      <iframe src="https://www.thiscodeworks.com/embed/605c46802476f700148cf8be" style={{ width: "70%", height: "23rem", display: "block", margin: "1rem auto", border: "none", minWidth: "23rem" }}></iframe>
     </div>
   );
 }
--- src/Components/ResponsiveImage/ResponsiveImage.js
+++ src/Components/ResponsiveImage/ResponsiveImage.js
@@ -22,11 +22,7 @@ function ResponsiveImage() {
                     </>
             </div>
             </div>
-            <iframe
-  src="https://carbon.now.sh/embed?bg=rgba%28216%2C217%2C218%2C1%29&t=blackboard&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=34px&ph=23px&ln=true&fl=1&fm=Fira+Code&fs=13px&lh=181%25&si=false&es=2x&wm=false&code=%2520%2520%253Cdiv%2520className%253D%2522hero-banner-three%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522https%253A%252F%252Fcdn.shopify.com%252Fs%252Ffiles%252F1%252F1367%252F5207%252Ffiles%252F07_01_Shop_mens_ad779213-0fed-41df-a3ab-2f5245a07264_1440x.jpg%253Fv%253D1615371691%2522%2520alt%253D%2522%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520className%253D%2522yoga-banner%2522%2520src%253D%2522https%253A%252F%252Fimages.pexels.com%252Fphotos%252F3094215%252Fpexels-photo-3094215.jpeg%253Fauto%253Dcompress%2526cs%253Dtinysrgb%2526dpr%253D1%2526w%253D500%2522%2520alt%253D%2522%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fdiv%253E"
-  style={{width:"1024px",height:"372px",border:"0",overflow:"hidden",display:"block",margin:"auto"}}
-  sandbox="allow-scripts allow-same-origin">
-</iframe>
+            <iframe src="https://www.thiscodeworks.com/embed/605c4c3a2476f700148cf8c7" style={{ width: "80%", height: "8rem", display: "block", margin: "1rem auto", border: "none", minWidth: "23rem" }}></iframe>
             </>
     )
 }
