b2a62f2d8a113f03ca162aae7d28b08010b396db
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-26 08:05:35 +0530

    UI-Fix Made changes to UI of Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,20 +4,20 @@ function Footer() {
   return (
     <div>
       <div className="footer-bottom">
-        <div className="footer-bottom-left">
-          <p className="footer-bottom-p-font">
-            © 2021 | Fit-Shaark Limited | All Rights Reserved @Akshay | About me
-          </p>
-        </div>
-        <div className="footer-bottom-right-container">
-          <div className="footer-bottom-right">
-            <i class="fab fa-twitter"></i>
-            <i class="fab fa-github"></i>
-            <i class="fab fa-linkedin"></i>
-            <i class="fas fa-envelope-square"></i>
-          </div>
-        </div>
+    <div className="footer-bottom-left-">
+      <p className="footer-bottom-p-font">
+        © 2021 | Fit-Shaark Limited | All Rights Reserved @Akshay | About me
+      </p>
+    </div>
+    <div className="footer-bottom-right-container">
+      <div className="footer-bottom-right">
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-github"></i>
+        <i class="fab fa-linkedin"></i>
+        <i class="fas fa-envelope-square"></i>
       </div>
+    </div>
+  </div>
 
       <div className="descrip" style={{ margin: "0rem 1rem" }}>
         <div className="heading">
